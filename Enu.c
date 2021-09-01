8e341d40c2207e52e93e2593f5a7660437bc2fc7
jac11 <rootx1982@gmail.com>
2021-09-01 22:45:06 +0400

    Update Enu.c

--- Enu.c
+++ Enu.c
@@ -1,262 +1,136 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 #include <time.h>
 #include <conio.h>
-#include "database.h"
+//#include "database.h"
 #include <string.h>
+#define PATH "%SYSTEMROOT%\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
+char  *end   = "________________________END________________________";
+char  *end1  = "___________________________________________________";
+char  *line0 = "=================================================";
+char  *line  = "==================================";
+char buf[BUFSIZ];
+FILE *file ;
+FILE *ptr;
 
 int time_c();
-int dir_file();
-int User_Enum();
-int route_network();
+char dir_file();
+char User_Enum();
+char  route_network();
+//int sys_info();
 //int system_info();
-int dir_file()
-   {
+char dir_file()
+{
+    char * log  = "=================================================\n"
+                  "              + Windows Enumeration +            \n"
+                  "=================================================\n"
+                  " + Windows Version and Configuration + \n"
+                  "=====================================\n\n";
+    char info [5][150] = {
+                    PATH  " systeminfo | findstr /B /C:OS",
+                    PATH  " wmic os get osarchitecture",
+                    PATH  " Get-ChildItem Env: || ft Key,Value",
+                    PATH  " wmic logicaldisk get caption,description,providername,Size",
+                    PATH  " wmic qfe"
+                    };
+          fprintf(file,"%s",log);
+          printf("%s",log);
+          for (int i =0 ;i<=5 ;i++){
+             if (!file) abort();
+             if ((ptr = popen(info[i], "r")) != NULL){
+                  while ((fgets(buf,BUFSIZ,ptr) != NULL)){
+                       fprintf(file,"%s", buf);
+                       printf("%s",buf);
+                  }
+             }
 
-           char * log  = "=================================================\n"
-                         "              + Windows Enumeration +            \n"
-                         "=================================================\n"
-                         " + Windows Version and Configuration + \n"
-                         "=====================================\n\n";
-
-          if (!file) abort();
-          if ((ptr = popen(Command, "r")) != NULL)
-          {
-                   fprintf(file,"%s",log);
-                   printf("%s",log);
-                   while ((fgets(buf,BUFSIZ,ptr) != NULL)){
+              printf("\n%s\n",end);fprintf(file,"%s\n",end);
+              printf("\n+ Configuration +\n%s\n",line);
+              if (i==4)
+                 break;
+         }
+  }
+char User_Enmu()
+  {
+      char * log  = "=================================================\n"
+                    "               + User  Enumeration +             \n"
+                    "=================================================\n"
+                    " + Get current username + \n"
+                    "=============================\n";
+      char info [8][200] = {
+                      PATH  " $env:username",
+                      PATH  " WHOAMI /PRIV /FO TABLE",
+                      PATH  " net user",
+                      PATH  " Get-ChildItem C:\\Users -Force || select Name",
+                      PATH  " net accounts",
+                      PATH  " net user administrator",
+                      PATH  " net user %USERNAME%",
+                      PATH  " net localgroup administrators"
+                      };
+            fprintf(file,"%s",log);
+            printf("%s",log);
+            for (int i =0 ;i<=8 ;i++){
+               if (!file) abort();
+               if ((ptr = popen(info[i], "r")) != NULL){
+                    while ((fgets(buf,BUFSIZ,ptr) != NULL)){
                          fprintf(file,"%s", buf);
                          printf("%s",buf);
-                         printf("%s\n",end1);fprintf(file,"%s\n",end1);
-                   }
-          printf("+ ARCHITECTURE +\n%s\n",line);
-          if ((ptr1 = popen(Command1, "r")) != NULL){
-                  fprintf(file,"\n%s\n%s\n","+ ARCHITECTURE +",line);
-                   while ((fgets(buf1,BUFSIZ,ptr1) != NULL)){
-                         fprintf(file,"%s",buf1);
-                         printf("%s",buf1);
-                   }
-                   printf("\n%s\n",end);fprintf(file,"%s\n",end);
-          }
-
-          printf("+ List all env variables +\n%s\n ",line);
-          fprintf(file,"\n%s\n%s\n","+ List all env variables +",line);
-          if ((ptr2 = popen(Command2, "r")) != NULL){
-                   while ((fgets(buf2, BUFSIZ,ptr2) != NULL)){
-                         fprintf(file, "%s",buf2);
-                         printf("%s",buf2);
-                  }
-                  printf("\n%s\n",end);fprintf(file,"%s\n",end);
-          }
-          printf("+ List all drives +\n%s\n",line);
-          fprintf(file,"\n%s\n%s\n","+ List all drives +",line);
-          if ((ptr3 = popen(Command3, "r")) != NULL){
-               while ((fgets(buf3,BUFSIZ,ptr3) != NULL)){
-                  fprintf(file, "%s",buf3);
-                  printf("%s",buf3);
-               }
-               printf("\n%s\n",end);fprintf(file,"%s\n",end);
-          }
-          printf("+ Extract patchs and updates  +\n%s\n",line);
-          fprintf(file,"\n%s\n%s\n","+ Extract patchs and updates +",line);
-          if ((ptr4 = popen(Command4, "r")) != NULL){
-               while ((fgets(buf4,BUFSIZ,ptr4) != NULL)){
-                  fprintf(file, "%s",buf4);
-                  printf("%s",buf4);
-               }
-          }
-               pclose(ptr);
-          }
-          fclose(file);
-   }
-
-int User_Enmu()
-{
-          // char buf2[BUFSIZ];
-           char * log  = "=================================================\n"
-                         "             + User  Enumeration +               \n"
-                         "=================================================\n"
-                         " + Get current username + \n"
-                         "=============================\n";
-           file = fopen("./Enum_Win.txt", "a");
-           fprintf(file,"%s",log);
-           if (!file) abort();
-           printf("%s",log);
-           if ((ptr = popen(Command5, "r")) != NULL) {
-                   while (fgets(buf,BUFSIZ, ptr) != NULL){
-                           printf("%s",buf);
-                           fprintf(file,"%s",buf);
-                  }
-                printf("\n%s\n",end);fprintf(file,"%s\n",end);
-            }
-            printf("+ Privilege +\n%s\n",line);
-            fprintf(file,"\n%s\n%s\n","+ Privilege +",line);
-            if((ptr1 = popen(Command6 ,"r"))!=NULL){
-                  while(fgets(buf1,BUFSIZ,ptr1)!=NULL){
-                     printf("%s",buf1);
-                     fprintf(file,"%s",buf1);
-                  }
-                  printf("\n%s\n",end);fprintf(file,"%s\n",end);
-            }
-            printf("+ List Of the Users +\n%s\n",line);
-            fprintf(file,"\n%s\n%s\n","+ List Of the Users +",line);
-            if((ptr2 = popen(Command7 ,"r"))!=NULL){
-                   while(fgets(buf2,BUFSIZ,ptr2)!=NULL){
-                           printf("%s",buf2);
-                           fprintf(file,"%s",buf2);
-                  }
-                  printf("\n%s\n",end);fprintf(file,"%s\n",end);
-            }
-            printf("+ Users Directory +\n%s\n",line);
-            fprintf(file,"\n%s\n%s\n","+ Users Directory +",line);;
-            if((ptr3 = popen(Command8 ,"r"))!=NULL){
-                   while(fgets(buf3,BUFSIZ,ptr3)!=NULL){
-                          printf("%s",buf3);
-                          fprintf(file,"%s",buf3);
                     }
-                     printf("\n%s\n",end);fprintf(file,"%s\n",end);
-            }
-            printf("+ logon requirements +\n%s\n",line);
-            fprintf(file,"\n%s\n%s\n","+ logon requirements +",line);;
-            if((ptr4 = popen(Command9 ,"r"))!=NULL){
-                     while(fgets(buf4,BUFSIZ,ptr4)!=NULL){
-                            printf("%s",buf4);
-                            fprintf(file,"%s",buf4);
+               }
+                printf("\n%s\n",end);fprintf(file,"%s\n",end);
+                printf("\n+ Configuration +\n%s\n",line);
+                if (i==7)
+                   break;
+           }
+    }
+char route_network()
+{
+      char * log  = "=================================================\n"
+                      "               + User  Enumeration +             \n"
+                      "=================================================\n"
+                      " + Get current username + \n"
+                      "=============================\n";
+      char info [9][200] = {
+                        PATH  " ipconfig /all",
+                        PATH  " Get-NetRoute -AddressFamily IPv4 || ft DestinationPrefix,NextHop,RouteMetric,ifIndex",
+                        PATH  " Get-NetNeighbor -AddressFamily IPv4 || ft ifIndex,IPAddress,LinkLayerAddress,State",
+                        PATH  " netstat -ano",
+                        PATH  " netsh firewall show config",
+                        PATH  " netsh advfirewall set allprofiles state off",
+                        PATH  " net share"
+                        PATH  " reg query HKLM\\SYSTEM\\CurrentControlSet\\Services\\SNMP /sf",
+                        PATH  " type %WINDIR%\\System32\\drivers\\etc\\hosts"
+                        };
+              fprintf(file,"%s",log);
+              printf("%s",log);
+              for (int i =0 ;i<=9 ;i++){
+                 if (!file) abort();
+                 if ((ptr = popen(info[i], "r")) != NULL){
+                      while ((fgets(buf,BUFSIZ,ptr) != NULL)){
+                           fprintf(file,"%s", buf);
+                           printf("%s",buf);
                       }
-                      printf("\n%s\n",end);fprintf(file,"%s\n",end);
-            }
-            printf("+ Administrator accont +\n%s\n",line);
-            fprintf(file,"\n%s\n%s\n","+ Administrator accont +",line);;
-            if((ptr5 = popen(Command10 ,"r"))!=NULL){
-                       while(fgets(buf5,BUFSIZ,ptr5)!=NULL){
-                              printf("%s",buf5);
-                              fprintf(file,"%s",buf5);
-                       }
-                       printf("\n%s\n",end);fprintf(file,"%s\n",end);
-            }
-            printf("+ User accont +\n%s\n",line);
-            fprintf(file,"\n%s\n%s\n","+ User accont +",line);
-            if((ptr7 = popen(Command11 ,"r"))!=NULL){
-                  while(fgets(buf7,BUFSIZ,ptr7)!=NULL){
-                        printf("%s",buf7);
-                        fprintf(file,"%s",buf7);
-                  }
+                 }
                   printf("\n%s\n",end);fprintf(file,"%s\n",end);
-            }
-            printf("+ localgroup administrators +\n%s\n",line);
-            fprintf(file,"\n%s\n%s\n","+ localgroup administrators +",line);
-            if((ptr8 = popen(Command12 ,"r"))!=NULL){
-                  while(fgets(buf8,BUFSIZ,ptr8)!=NULL){
-                        printf("%s",buf8);
-                        fprintf(file,"%s",buf8);
-                  }
+                  printf("\n+ Configuration +\n%s\n",line);
+                  if (i==8)
+                     break;
             }
             pclose(ptr);
             fclose(file);
+            return 0;
 }
-int route_network()
-{
-      char * log  = "=================================================\n"
-                    "             + Network Enumeration +             \n"
-                    "=================================================\n"
-                    " + IP Configuration +        \n"
-                    "=============================\n";
-      file = fopen("./Enum_Win.txt", "a");
-      printf("%s",log);
-      fprintf(file,"%s",log);
-      if((ptr1 = popen(Command13 ,"r"))!=NULL){
-             while(fgets(buf1,BUFSIZ,ptr1)!=NULL){
-                   printf("%s",buf1);
-                   fprintf(file,"%s",buf1);
-            }
-            printf("\n%s\n",end);fprintf(file,"%s\n",end);
-      }
-      printf("+ router tabel +\n%s\n",line);
-      fprintf(file,"\n%s\n%s\n","+ router tabel +",line);
-       if((ptr2 = popen(Command14 ,"r"))!=NULL){
-                while(fgets(buf2,BUFSIZ,ptr2)!=NULL){
-                     printf("%s",buf2);
-                    fprintf(file,"%s",buf2);
-                }
-          printf("\n%s\n",end);fprintf(file,"%s\n",end);
-      }
-      printf("+ ARP table +\n%s\n",line);
-      fprintf(file,"\n%s\n%s\n","+ ARP tablel +",line);
-       if((ptr3 = popen(Command15 ,"r"))!=NULL){
-                while(fgets(buf3,BUFSIZ,ptr3)!=NULL){
-                     printf("%s",buf3);
-                    fprintf(file,"%s",buf3);
-                }
-          printf("\n%s\n",end);fprintf(file,"%s\n",end);
-      }
-      printf("+ current connections +\n%s\n",line);
-      fprintf(file,"\n%s\n%s\n","+ current connections +",line);
-       if((ptr4 = popen(Command16 ,"r"))!=NULL){
-                while(fgets(buf4,BUFSIZ,ptr4)!=NULL){
-                     printf("%s",buf4);
-                    fprintf(file,"%s",buf4);
-                }
-          printf("\n%s\n",end);fprintf(file,"%s\n",end);
-      }
-      printf("+ firewall state and current configuration +\n%s\n",line);
-      fprintf(file,"\n%s\n%s\n","+ firewall state and current configuration +",line);
-       if((ptr5 = popen(Command17 ,"r"))!=NULL){
-                while(fgets(buf5,BUFSIZ,ptr5)!=NULL){
-                     printf("%s",buf5);
-                    fprintf(file,"%s",buf5);
-                }
-          printf("\n%s\n",end);fprintf(file,"%s\n",end);
-      }
-      /*
-      printf("+ firewall’s blocked ports +\n%s\n",line);
-      fprintf(file,"\n%s\n%s\n","+ firewall’s blocked ports +",line);
-       if((ptr6 = popen(Command18 ,"r"))!=NULL){
-                while(fgets(buf6,BUFSIZ,ptr6)!=NULL){
-                     printf("%s",buf6);
-                    fprintf(file,"%s",buf6);
-                }
-          printf("\n%s\n",end);fprintf(file,"%s\n",end);
-      }
-      */
-      printf("+ Disable firewall +\n%s\n",line);
-      fprintf(file,"\n%s\n%s\n","+ Disable firewall +",line);
-       if((ptr7 = popen(Command19 ,"r"))!=NULL){
-                while(fgets(buf7,BUFSIZ,ptr7)!=NULL){
-                     printf("%s",buf7);
-                    fprintf(file,"%s",buf7);
-                }
-          printf("\n%s\n",end);fprintf(file,"%s\n",end);
-      }
-      printf("+ List all network shares +\n%s\n",line);
-      fprintf(file,"\n%s\n%s\n","+ List all network shares +",line);
-       if((ptr8 = popen(Command20 ,"r"))!=NULL){
-                while(fgets(buf8,BUFSIZ,ptr8)!=NULL){
-                     printf("%s",buf8);
-                    fprintf(file,"%s",buf8);
-                }
-          printf("\n%s\n",end);fprintf(file,"%s\n",end);
-      }
-      printf("+ SNMP Configuration +\n%s\n",line);
-      fprintf(file,"\n%s\n%s\n","+ SNMP Configuration +",line);
-       if((ptr9 = popen(Command21 ,"r"))!=NULL){
-            while(fgets(buf9,BUFSIZ,ptr9)!=NULL){
-                  printf("%s",buf9);
-                  fprintf(file,"%s",buf9);
-            }
-             printf("\n%s\n",end);fprintf(file,"%s\n",end);
-      }
-      printf("+ Host file Configuration +\n%s\n",line);
-      fprintf(file,"\n%s\n%s\n","+ Host file Configuration +",line);
-       if((ptr10 = popen(Command22 ,"r"))!=NULL){
-            while(fgets(buf10,BUFSIZ,ptr10)!=NULL){
-                  printf("%s",buf10);
-                  fprintf(file,"%s",buf10);
-            }
-             printf("\n%s\n",end);fprintf(file,"%s\n",end);
-      }
-      pclose(ptr);
-      fclose(file);
+int time_c(){
+       time_t timeNow;
+       timeNow = time(NULL);
+       char *TIMENOW= (ctime(&timeNow));
+       file = fopen("./Enum_Win.txt", "w+");
+       fprintf(file,"%s\n", line0);
+       fprintf(file,"%s","Currant Time&Date : ");
+       fprintf(file,"%s", TIMENOW);
+       printf("%s\nCurrent Time&Date : %s",line0, TIMENOW);
 }
 
 int main(){
